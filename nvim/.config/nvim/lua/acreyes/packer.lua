946a13acb667a57b1102ce7558c0372ace70a02e
adam reyes <adam.c.reyes@gmail.com>
2023-04-18 10:42:14 -0400

    local updates from Turon

--- nvim/.config/nvim/lua/acreyes/packer.lua
+++ nvim/.config/nvim/lua/acreyes/packer.lua
@@ -4,63 +4,63 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use( 'nvim-treesitter/playground')
-  use( 'nvim-treesitter/nvim-treesitter-context' )
-  use( 'ThePrimeagen/harpoon')
-  use( 'mbbill/undotree')
-  use( 'tpope/vim-fugitive')
-  use( 'tpope/vim-surround')
-  use( 'tpope/vim-commentary')
-  use( 'Civitasv/cmake-tools.nvim')
-  use( 'mfussenegger/nvim-dap')
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-  use "folke/neodev.nvim"
-  use {
-      "williamboman/mason.nvim",
-      run = ":MasonUpdate" -- :MasonUpdate updates registry contents
-  }
-  use ("jay-babu/mason-nvim-dap.nvim")
-  use( 'vim-pandoc/vim-pandoc')
-  use( 'vim-pandoc/vim-pandoc-syntax')
-  use( 'christoomey/vim-tmux-navigator')
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use( 'nvim-treesitter/playground')
+    use( 'nvim-treesitter/nvim-treesitter-context' )
+    use( 'ThePrimeagen/harpoon')
+    use( 'mbbill/undotree')
+    use( 'tpope/vim-fugitive')
+    use( 'tpope/vim-surround')
+    use( 'tpope/vim-commentary')
+    use( 'Civitasv/cmake-tools.nvim')
+    use( 'mfussenegger/nvim-dap')
+    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+    use "folke/neodev.nvim"
+    use {
+        "williamboman/mason.nvim",
+        run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+    }
+    use ("jay-babu/mason-nvim-dap.nvim")
+    use( 'vim-pandoc/vim-pandoc')
+    use( 'vim-pandoc/vim-pandoc-syntax')
+    use( 'christoomey/vim-tmux-navigator')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
 }
 
 
