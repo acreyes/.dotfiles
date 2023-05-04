dcc31369e1420a6e8908c75219ecc6c0df397944
Adam C Reyes <adam.c.reyes@gmail.com>
2023-05-04 14:23:05 -0400

    Update env.zsh

--- ohmyszh/.oh-my-zsh/custom/env.zsh
+++ ohmyszh/.oh-my-zsh/custom/env.zsh
@@ -5,6 +5,5 @@ export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 export REPO_DIR=/Users/adamreyes/Documents/research/repos/PAS
 export RSCH_DIR=/Users/adamreyes/Documents/research
 
-export SLACK_TKN=xoxb-1227737473766-1293797827857-x6d6r7mmHTeAdbJhCTJsPgI0
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
