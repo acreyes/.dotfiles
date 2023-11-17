b8598c7bc265513137c7db1e5a2d677cab2415cd
adam reyes <adam.c.reyes@gmail.com>
2023-11-17 22:23:55 +0100

    updated robodoc snippet to handle user derived types

--- nvim/.config/nvim/after/plugin/robodoc_snip.lua
+++ nvim/.config/nvim/after/plugin/robodoc_snip.lua
@@ -20,6 +20,7 @@ local myTypes = vim.treesitter.query.parse("fortran",
    [[
       (intrinsic_type)@type
       (type_qualifier)@qual
+      (derived_type)@derT
       (identifier)@id
    ]])
 
@@ -29,14 +30,19 @@ local myDeclarations = vim.treesitter.query.parse("fortran",
    ]]
 )
 
+local getTableSize = function(t)
+    local count = 0
+    for _, __ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
+
 local localPath = function()
    local path = vim.fn.expand('%')
    local source = vim.split(path, ".", true)
-   print(source[1])
    local parts = vim.split(source[1],"/",true)
-   print(parts[#parts])
    name = parts[#parts] or ""
-   print(name)
    local internal = "if"
    if name:sub(1,1) == string.upper(name:sub(1,1)) then
       internal = "f"
@@ -65,7 +71,6 @@ local getSynopsis = function(opts)
    for child in root:iter_children() do
       if child:type() ~= "comment" then
          for node in child:iter_children() do
-            print(node:type())
             if node:type() == "subroutine_statement" then
                 subroutine = true
              elseif node:type() == "module_statement" then
@@ -76,15 +81,12 @@ local getSynopsis = function(opts)
       end
    end
 
-   local parts = vim.split(localPath(),"/",true)
-   local name = parts[#parts] or ""
-
    if subroutine then
       for id, node in myParameters:iter_captures(root, bufnr, 0, -1) do
          local parm = vim.treesitter.get_node_text(node, bufnr)
          parameters[parm] = parm
       end
-      if #parameters == 0 then
+      if getTableSize(parameters) == 0 then
          return sn(1, t("!! call "..name.."()"))
       end
       local parTypes = {}
@@ -94,10 +96,10 @@ local getSynopsis = function(opts)
          local parm = {}
          local qual = nil
          local cType = nil
+         local dType = nil
          local dimension = nil
          local intent = nil
          for cid, cnode in myTypes:iter_captures(node, bufnr, 0, -1) do
-            -- print(vim.inspect(vim.treesitter.get_node_text(cnode, bufnr)))
 
             if myTypes.captures[cid] == 'id' then
                local _parm = vim.treesitter.get_node_text(cnode,bufnr)
@@ -111,6 +113,8 @@ local getSynopsis = function(opts)
                end
             elseif myTypes.captures[cid] == 'type' then
                cType = vim.treesitter.get_node_text(cnode,bufnr)
+            elseif myTypes.captures[cid] == 'derT' then
+               dType = vim.treesitter.get_node_text(cnode,bufnr)
             end
          end
          for _, p in pairs(parm) do
@@ -119,7 +123,11 @@ local getSynopsis = function(opts)
                if dimension then
                   pardim = p .. string.upper(dimension)
                end
-               parTypes[pardim] = cType .. string.upper(intent)
+               if dType then
+                  parTypes[pardim] = dType .. string.upper(intent)
+               elseif cType then
+                  parTypes[pardim] = cType .. string.upper(intent)
+               end
             end
          end
       end
@@ -129,7 +137,6 @@ local getSynopsis = function(opts)
       local fullText = ""
       local didText = nil
       for parm, descr in pairs(parTypes) do
-         print(vim.inspect({p=parm,d=descr}))
          table.insert(nodes, t(string.rep(" ", indent) .. string.format("%-12s",descr) .. " :: " .. parm))
          if indent == 0 then
             indent = #text-2
@@ -142,7 +149,6 @@ local getSynopsis = function(opts)
          end
       end
       fullText = fullText .. ")"
-      print(vim.inspect(fullText))
       return sn(1, fmt(fullText, nodes))
    end
 
