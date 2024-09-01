6ff1be8388d4f2692fca7353dfbce3ee0bb686d3
adam.c.reyes <adam.reyes@rochester.edu>
2024-09-01 12:12:05 -0400

    remove problematic treesitter languages

--- nvim/.config/nvim/lua/acreyes/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/acreyes/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
    config = function()
       require'nvim-treesitter.configs'.setup {
          -- A list of parser names, or "all" (the five listed parsers should always be installed)
-         ensure_installed = { "vimdoc", "c", "lua", "vim", "query", "fortran", "cpp", "dockerfile", "latex", "zig" },
+         ensure_installed = { "vimdoc", "c", "lua", "vim", "query", "fortran", "cpp", "dockerfile"},
 
          -- Install parsers synchronously (only applied to `ensure_installed`)
          sync_install = false,
